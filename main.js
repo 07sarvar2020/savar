25ee015a6927db000fdafc9164b0e3d931d185c8
Sarvar <sarvar.@mail.ru>
2021-12-25 13:33:53 +0500

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function tanishuv(name, year, god, car) {
+    return `Mani ismim ${name}. Hozir ${year} yil. Man ${god} yilman. Mani moshinam bor ${car} markasi yoqadi`
+}
+alert(tanishuv('Ivan','2021' , '1992' , 'Tesla'));
+
+function  yosh ( year , god) {
+    return `Man yoshim ${year-god} da orzularga yetish shart ;`
+}
+alert(yosh(2021, 1992, ));
+
+ 
